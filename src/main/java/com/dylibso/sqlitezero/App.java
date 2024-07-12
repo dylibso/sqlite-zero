85ec998f41de6a8e1fa219b75b2a690ddfe30ed1
Benjamin Eckel <bhelx@simst.im>
2024-07-12 10:16:07 -0500

    fix

--- src/main/java/com/dylibso/sqlitezero/App.java
+++ src/main/java/com/dylibso/sqlitezero/App.java
@@ -17,13 +17,13 @@ public class App
         var db = new Database(databasePath).open();
         var results = new SqlResults<Track>();
         var sql = """
-                SELECT TrackId, Name, Composer FROM track WHERE id = 3505;
+                SELECT TrackId, Name, Composer FROM Track WHERE Composer like '%Glass%';
                 """;
         var start = System.nanoTime();
         db.exec(sql, results);
         var rows = results.cast(Track.class);
         var end = System.nanoTime() - start;
-        System.out.println("NANOS: " + (end / 1000000000.0));
+        System.out.println("Queried in seconds: " + (end / 1000000000.0));
         for (var r : rows) {
             System.out.println(r);
         }
